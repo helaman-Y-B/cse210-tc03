4e18fb9fa9e8b1b37d572d29fd88f77c0d82a3b5
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2021-10-02 12:06:29 -0300

    Adding greeatings
    Everything from us

--- assigments/colaborative.py
+++ assigments/colaborative.py
@@ -1,10 +1,14 @@
 from helama import greeting, little
+from paredes import nicolas
+from salva import my_greetings
 
 
 def main():
     print("Welcome to colaborative program!")
     print("something")
     greeting()
+    nicolas()
+    my_greetings()
     math = little()
     print(math)
 
