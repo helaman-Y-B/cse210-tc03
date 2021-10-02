17e11518c5168e56f34d77f73204b102d116d2d2
helaman-Y-B <helamashi@gmail.com>
2021-10-02 11:54:13 -0300

    Helama Greeting

--- assigments/colaborative.py
+++ assigments/colaborative.py
@@ -1,2 +1,13 @@
-print("Welcome to colaborative program!")
-print("something")
+from helama import greeting, little
+
+
+def main():
+    print("Welcome to colaborative program!")
+    print("something")
+    greeting()
+    math = little()
+    print(math)
+
+
+if __name__ == "__main__":
+    main()
