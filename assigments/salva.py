ca7d6c8f9685253ed924919e71543d70d3cfca6a
James Charlie Salva <JamesSalvajames@hotmail.com>
2021-10-02 22:49:38 +0800

    this is my work for the second part

---
+++ assigments/salva.py
@@ -0,0 +1,14 @@
+
+def my_greetings():
+
+    first_number = 1234
+    second_number = 5678
+    print("Hello from James Charlie Salva")
+    print(f"I can multiply numbers. For example: {first_number} x {second_number} = {first_number*second_number}")
+
+def main():
+    my_greetings()
+
+
+if __name__ == "__main__":
+    main()
